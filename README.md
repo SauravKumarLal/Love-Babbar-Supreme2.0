c8c8a385052174575701cd72812eaf1efe8f9951
SauravKumarLal <sauravkrlal@gmail.com>
2023-09-20 00:32:23 +0530

    added KMtoMiles and testing/learning git, also modified readme

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Here, I'll be uploading all the code solutions of Love Babbar Supreme 2.0 exercises!
 
 Language Use - C++
+
+Author - Saurav Kumar Lal
---
+++ Week 2/KMtoMiles.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float km;
+    cin>>km;
+
+    float miles=km*0.621371;
+
+    cout<<km<<" kms is equal to "<<miles<<" miles";
+}
