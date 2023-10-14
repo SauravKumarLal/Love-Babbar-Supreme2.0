e0ac87df3c275ebb3c88defc183cf8fd0ee7baa6
SauravKumarLal <sauravkrlal@gmail.com>
2023-10-14 23:22:16 +0530

    small edit

--- Week 6/optimization_of_sieveOfEratosthenes.cpp
+++ Week 6/optimization_of_sieveOfEratosthenes.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 #include<vector>
+#include<math.h>
 using namespace std;
 
 vector<bool> Sieve(int n){
     vector<bool> sieve(n+1, true);
     sieve[0]=sieve[1]=false;
 
-    for(int i=2; i<=n; i++){
+    for(int i=2; i<=sqrt(n); i++){ //optimization 2: or i*i=n
         if(sieve[i] == true){
 
             int j=i*i; //optimization 1: first unmarked no. would be i*i, as others have been marked by 2 to i
