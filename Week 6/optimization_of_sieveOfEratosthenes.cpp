575a31a14629ed27f05257b3e45f19effbf2615e
SauravKumarLal <sauravkrlal@gmail.com>
2023-10-14 23:15:38 +0530

    okay

---
+++ Week 6/optimization_of_sieveOfEratosthenes.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<bool> Sieve(int n){
+    vector<bool> sieve(n+1, true);
+    sieve[0]=sieve[1]=false;
+
+    for(int i=2; i<=n; i++){
+        if(sieve[i] == true){
+
+            int j=i*i; //optimization 1: first unmarked no. would be i*i, as others have been marked by 2 to i
+            while(j<=n){
+                sieve[j]=false;
+                j+=i;
+            }
+        }
+    }
+    return sieve;
+}
+
+int main(){
+    vector<bool> sieve = Sieve(25);
+    for(int i=0; i<=25; i++){
+        if(sieve[i]){
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}
