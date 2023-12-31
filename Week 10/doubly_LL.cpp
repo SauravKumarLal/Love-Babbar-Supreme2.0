cb75f571b3465239cb614c794a8fc7aaeaa7dd42
SauravKumarLal <sauravkrlal@gmail.com>
2023-12-31 16:39:54 +0530

    correct findLength fn..

--- Week 10/doubly_LL.cpp
+++ Week 10/doubly_LL.cpp
@@ -70,6 +70,35 @@ void insertAtTail(Node* &head, Node* &tail, int data){
     }
 }
 
+void insertAtPosition(Node* &head, Node* &tail, int data, int position){
+    int len = findLength(head);
+
+    if(position == 1){
+        insertAtHead(head, tail, data);
+        return;
+    }
+
+    else if(position == len){
+        insertAtTail(head, tail, data);
+        return;
+    }
+    else{
+        Node* newNode = new Node(data);
+        Node* previous = NULL;
+        Node* curr = head; 
+
+        while(position != 1){
+            position--;
+            previous = curr;
+            curr = curr->next;
+        }
+        newNode->next = curr;
+        curr->prev = newNode;
+        newNode->prev = previous;
+        previous->next = newNode;
+    }
+}
+
 int main(){
     // Node* head = new Node(10);
     Node *head = NULL;
@@ -79,4 +108,11 @@ int main(){
     insertAtHead(head, tail, 22);
     insertAtHead(head, tail, 11);
     Print(head);
+    cout<<endl;
+    cout<<"length is: "<<findLength(head);
+    cout<<endl;
+    // insertAtTail(head, tail, 111);
+    insertAtPosition(head, tail, 111, 5);
+    Print(head);
+
 }
