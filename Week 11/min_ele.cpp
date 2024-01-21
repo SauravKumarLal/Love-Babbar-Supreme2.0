959a5c43a698007e8265a9ccb25b22a9d5f76e2a
SauravKumarLal <sauravkrlal@gmail.com>
2024-01-21 18:34:21 +0530

    practice

---
+++ Week 11/min_ele.cpp
@@ -0,0 +1,36 @@
+//transverse right->left
+
+#include<iostream>
+#include<stack>
+#include<vector>
+
+using namespace std;
+
+vector<int> nextSmallElement(int *arr, int size, vector<int> &ans){
+    stack<int> st;
+    st.push(-1);
+
+    for(int i=size-1; i>=0; i--){
+        int curr = arr[i];
+        //find ans for curr
+        while(st.top()>=curr){
+            st.pop();
+        }
+        ans[i] = st.top();
+
+        st.push(curr);
+    } 
+    return ans;
+}
+
+int main(){
+    int arr[5] = {8, 4, 6, 2, 3};
+    int size = 5;
+    vector<int> ans(size);
+    ans = nextSmallElement(arr, size, ans);
+    for(auto i: ans){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
